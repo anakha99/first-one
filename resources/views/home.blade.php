d1539ae12c723c2eff555f86f3f3a02d4bf1da57
Anakha <anakha08051999@gmail.com>
2022-09-05 17:16:47 +0530

    initial cmt

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOME Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, placeat, consequatur illum tempora, earum cupiditate dicta quibusdam suscipit doloribus molestiae velit fugiat assumenda maiores? Aspernatur sint exercitationem tenetur debitis laboriosam.</p>
+</body>
+</html>
